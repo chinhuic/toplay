c933519231ee32d51d411a390e6e1d586082ef3b
chinhuic <chinhuic@gmail.com>
2016-09-03 00:41:29 -0400

    To add Tung Tung in there

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask("toplay")
 import random
 
-choices=['Hilary','Trump','Jill','Gary']
+choices=['Hilary','Trump','Jill','Gary','Chin','Tung Tung']
 
 @app.route('/')
 def is_nuts():
